d7454a43e17b9eab25b2802678955d13a2c4fc0b
WenhaiZhu <944284742@qq.com>
2022-10-29 17:37:14 +0800

    增加智能指针

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <memory>
+
+using namespace std;
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+    shared_ptr<string> p1 = make_shared<string>(10, '9'); 
+    unique_ptr<int> iPtr = make_unique<int>(10,8);
+    cout<<*iPtr << endl << *p1 << endl;    //输出：8
 }
